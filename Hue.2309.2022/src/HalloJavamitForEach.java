cad2bfa8f2b44712d4c9e75b7f076b7a411588ec
Thiimoo <timo.reitboeck@gmx.at>
2022-09-23 12:00:24 +0200

    Aufgabe 1 Nr 1

--- Hue.2309.2022/src/HalloJavamitForEach.java
+++ Hue.2309.2022/src/HalloJavamitForEach.java
@@ -1,7 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class HalloJavamitForEach {
 
 
     public static void main(String[] args) {
+        List<String> stringList = new ArrayList<>();
+        stringList.add("Hallo");
+        stringList.add("Welt");
 
+        for (String s : stringList)
+        {
+            System.out.print(s);
+            System.out.print(" ");
+        }
     }
 }
